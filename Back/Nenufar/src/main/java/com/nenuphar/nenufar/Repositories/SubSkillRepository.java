e4ee46c9d8dc4f94ed4ce5d2e0e457b0a2f1cd45
Alexvanwee <alexandre.vanwesel@gmail.com>
2018-01-16 18:43:18 +0100

    getSubSkillsFromSkillID()

--- Back/Nenufar/src/main/java/com/nenuphar/nenufar/Repositories/SubSkillRepository.java
+++ Back/Nenufar/src/main/java/com/nenuphar/nenufar/Repositories/SubSkillRepository.java
@@ -1,7 +1,12 @@
 package com.nenuphar.nenufar.Repositories;
 
 import com.nenuphar.nenufar.Models.SubSkill;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SubSkillRepository extends CrudRepository<SubSkill, Long> {
+    @Query(value="SELECT * FROM sub_skill WHERE skill_id = ?1", nativeQuery = true)
+    List<SubSkill> getSubSkillsFromSkillID(int skill_id);
 }
--- Back/Nenufar/src/main/java/com/nenuphar/nenufar/Services/SubSkillService.java
+++ Back/Nenufar/src/main/java/com/nenuphar/nenufar/Services/SubSkillService.java
@@ -5,6 +5,8 @@ import com.nenuphar.nenufar.Repositories.SubSkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SubSkillService {
 
@@ -20,5 +22,18 @@ public class SubSkillService {
         SubSkill subskill = new SubSkill(name, grade);
         subSkillRepository.save(subskill);
         return subskill;
+    }
+
+    public List<SubSkill> getSubSkillsFromSkillID(int skill_id)
+    {
+        try
+        {
+            List<SubSkill> subskills = subSkillRepository.getSubSkillsFromSkillID(skill_id);
+            return subskills;
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
     }
 }
